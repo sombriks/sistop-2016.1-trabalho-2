72cf6f8f1453bba901c6322c2e42f3f0de805104
sombriks <sombriks@gmail.com>
2016-09-23 16:12:14 -0300

    W.I.P.

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ var config = {
   memorysizemb: 100,// tamanho da memória de simulação
   sistopmemorymb: 10,// memória gasta com o SO
   processmemoryrangemb: [1, 35],// ranges de tamanho de memória dos processos
-  timebetweenprocrangesec: [1, 10],// range de tempo entre as alocações
+  timeproccreationrangesec: [1, 10],// range de tempo de criação de cada processo'
   timeprocdurationrangesec: [3, 30],// range de duração de cada processos
   simulationstepsec: 1// sleep da simulação em segundos
 };
@@ -22,18 +22,20 @@ var simulation = {
   physicalmemory: [],
   osmemory: [],
   memorymap: {},
-  lastallocation:0,
+  lastallocation: 0,
+  strategy: null,
   init: function (config) {
-    
+
     // select the strategy to use 
-    simulation.strategy=config.strategies[config.currentstrategy];
-    
+    simulation.strategy = config.strategies[config.currentstrategy];
+
     // all the process that will ever exists
     var i = config.numprocesses;
     while (i--) {
       simulation.processqueue.push({
-        queuedat: new Date().getTime(),
-        duration: util.rangeutil(config.timeprocdurationrangesec),
+        queued_at: new Date(),
+        duration_time: util.rangeutil(config.timeprocdurationrangesec) * 1000,
+        creation_time: util.rangeutil(config.timeproccreationrangesec) * 1000,
         memoryused: util.rangeutil(config.processmemoryrangemb)
       });
     }
@@ -63,14 +65,25 @@ var simulation = {
       memoend: config.memorysizemb - 1
     };
   },
-  step:function(){
-    if(simulation.running){
-      // how to simulate: 
-      // 1 - take one process from the simulation.processqueue
-      
-      // 2 - check if now is greater (later) than last allocation + now
-      // 2.1 - if not, yeld
-      // 2.2 - if yes, pop() the queue 
+  step: function () {
+    if (simulation.running) {
+      var proc = simulation.processqueue.pop()
+      if (proc) {
+        if (simulation.lastallocation + proc.creation_time <= new Date().getTime()) {
+          if(util.allocate(proc,simulation)){
+            proc.created_at = new Date(proc.creation_time + new Date().getTime());
+            simulation.lastallocation = proc.created_at.getTime();
+            console.log(proc)
+          }else{
+            // unable to alloc, try again later
+            simulation.processqueue.push(proc);
+          }
+        } else {
+          simulation.processqueue.push(proc);
+        }
+      }else{
+        console.log("queue empty");
+      }
       // 3 - look for allocation strategy
       // 3.1 - if there is no available node according to memory map, push() back and yeld 
       // 3.1 - if there is a node, allocate process and add it to memory map
@@ -92,4 +105,6 @@ simulation.step();
 
 console.log(simulation);
 
-window.simulation=simulation;
+window.simulation = simulation;
+
+simulation.running=true;
--- src/util.js
+++ src/util.js
@@ -1,8 +1,41 @@
 
-module.exports = {
+var utilities = {
   rangeutil: function (range) {
     var a = range[0];
     var b = range[1];
     return Math.floor(a + Math.random() * (b - a));
+  },
+  allocate: function (proc, simu) {
+    switch (simu.strategy) {
+      case "first fit":
+        return utilities.firstfit(proc, simu);
+      case "best fit":
+        return utilities.bestfit(proc, simu);
+      case "worst fit":
+        return utilities.worstfit(proc, simu);
+      case "next fit":
+        return utilities.nextfit(proc, simu);
+      default:
+        console.log("no allocation strategy defined!");
+        return 0;
+    }
+  },
+  firstfit: function (proc, simu) {
+    console.log("using first fit:");
+    return 1;
+  },
+  bestfit: function (proc, simu) {
+    console.log("using best fit:");
+    return 1;
+  },
+  worstfit: function (proc, simu) {
+    console.log("using worst fit:");
+    return 1;
+  },
+  nextfit: function (proc, simu) {
+    console.log("using next fit:");
+    return 1;
   }
 }
+
+module.exports = utilities;
